a4ceff0ab97376b3301fadba0efb8412be442ecd
masha1321 <mariazajko788@gmail.com>
2024-04-15 15:00:33 +0300

    Task5

---
+++ Application_A/Task5/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите радиус основания цилиндра: ");
+            double r = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите высоту цилиндра: ");
+            double h = Convert.ToDouble(Console.ReadLine());
+
+            double volume = Math.PI * Math.Pow(r, 2) * h;
+
+            Console.WriteLine($"Объем цилиндра: {volume}");
+        }
+    }
+}
