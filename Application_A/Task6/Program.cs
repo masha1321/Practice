c93c4f772f9e4d02bfd1a593323f87df3c39aee6
masha1321 <mariazajko788@gmail.com>
2024-04-15 15:04:24 +0300

    Task6

---
+++ Application_A/Task6/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Task6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите трехзначное число: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int firstDigit = number / 100;
+            int secondDigit = (number / 10) % 10;
+            int thirdDigit = number % 10;
+
+            int product = firstDigit * secondDigit * thirdDigit;
+
+            Console.WriteLine("Произведение цифр числа: " + product);
+        }
+    }
+}
