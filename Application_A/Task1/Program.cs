68cceebcc27f36d78fec59c84f5c77fd6460f080
masha1321 <mariazajko788@gmail.com>
2024-04-15 14:25:41 +0300

    Task2

--- Application_A/Task1/Program.cs
+++ Application_A/Task1/Program.cs
@@ -1,18 +1,17 @@
-﻿using System;
-namespace Application_A
+﻿namespace Task1
 {
-    internal class Task1
+    internal class Program
     {
         static void Main(string[] args)
         {
-          
+
             Console.Write("Введите первое вещественное число: ");
             double num1 = double.Parse(Console.ReadLine());
             Console.Write("Введите второе вещественное число: ");
             double num2 = double.Parse(Console.ReadLine());
             double result = num2 / num1;
 
-           
+
             Console.WriteLine($"Результат деления: {result:F2}");
         }
     }
---
+++ Application_A/Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.Write("Введите четырехзначное число: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number < 1000 || number > 9999)
+            {
+                Console.WriteLine("Введено некорректное число. Пожалуйста, введите четырехзначное число.");
+                return;
+            }
+
+            int digit1 = number % 10;
+            int digit2 = (number / 10) % 10;
+            int digit3 = (number / 100) % 10;
+            int digit4 = (number / 1000) % 10;
+
+            
+            int reversedNumber = digit1 * 1000 + digit2 * 100 + digit3 * 10 + digit4;
+
+            Console.WriteLine($"Число, полученное при прочтении цифр справа налево: {reversedNumber}");
+        }
+    }
+}
