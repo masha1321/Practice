776d2ff874960be6f5442641911f2ba1a92786da
masha1321 <mariazajko788@gmail.com>
2024-04-15 14:55:54 +0300

    Task4

---
+++ Application_A/Task4/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите радиус основания цилиндра: ");
+            double r = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите высоту цилиндра: ");
+            double h = Convert.ToDouble(Console.ReadLine());
+
+            double volume = Math.PI * Math.Pow(r, 2) * h;
+            double surfaceArea = 2 * Math.PI * r * (r + h);
+
+            Console.WriteLine($"Объем цилиндра: {volume}");
+            Console.WriteLine($"Площадь поверхности цилиндра: {surfaceArea}");
+        }
+    }
+}
