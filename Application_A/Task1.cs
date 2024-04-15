acf47817c39270935a0300fa7359ada708f4755d
masha1321 <mariazajko788@gmail.com>
2024-04-15 14:05:12 +0300

    complete Task1

---
+++ Application_A/Task1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Application_A
+{
+    internal class Task1
+    {
+        static void Main(string[] args)
+        {
+          
+            Console.Write("Введите первое вещественное число: ");
+            double num1 = double.Parse(Console.ReadLine());
+            Console.Write("Введите второе вещественное число: ");
+            double num2 = double.Parse(Console.ReadLine());
+            double result = num2 / num1;
+
+           
+            Console.WriteLine($"Результат деления: {result:F2}");
+        }
+    }
+}
---
+++ Application_A/Task2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application_A
+{
+    internal class Task2
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
