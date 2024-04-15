64591c7ee4b82a832894d201f1bbc4e306357acb
masha1321 <mariazajko788@gmail.com>
2024-04-15 14:46:27 +0300

    Task3

---
+++ Application_A/Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 1;
+            double y = Math.Pow(x, 2) * Math.Sqrt(1 - Math.Pow(Math.Sin(x), 2)) * Math.Exp(4 * Math.PI * x) / Math.Cos(2 * Math.PI / 3);
+            Console.WriteLine("Значение функции равно: " + y);
+        }
+    }
+}
