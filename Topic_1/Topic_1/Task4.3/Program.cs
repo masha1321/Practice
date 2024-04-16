9bf9cb03aa446f715d429368650676380468035e
masha1321 <mariazajko788@gmail.com>
2024-04-16 13:37:31 +0300

    Task4.3

---
+++ Topic_1/Topic_1/Task4.3/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Task4._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 10;
+            int B = 20; 
+
+            for (int i = A; i <= B; i++)
+            {
+                if (i % 10 % 2 == 0) 
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
