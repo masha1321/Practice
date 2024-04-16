1c5e102b807725e5c6c74388fd86846652f4de5a
masha1321 <mariazajko788@gmail.com>
2024-04-16 12:32:12 +0300

    Task1

---
+++ Topic_1/Topic_1/Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите значение x: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y;
+
+            if (x >= 0.1 && x <= 1.5)
+            {
+                y = Math.Log(Math.Pow(Math.E, x + 4)) - 2 * x;
+            }
+            else if (x > 1.5)
+            {
+                y = Math.Pow(x, 2) - 1;
+            }
+            else
+            {
+                Console.WriteLine("Значение x вне определенного диапазона.");
+                return;
+            }
+
+            Console.WriteLine($"Значение y для данного x равно: {y}");
+        }
+    }
+}
