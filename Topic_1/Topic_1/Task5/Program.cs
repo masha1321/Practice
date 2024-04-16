b8f950a95494316bcb8de7ae6c064de426d95e21
masha1321 <mariazajko788@gmail.com>
2024-04-16 13:53:19 +0300

    Task5

---
+++ Topic_1/Topic_1/Task5/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите цену 1 кг конфет: ");
+            double price = Convert.ToDouble(Console.ReadLine());
+
+            for (double weight = 0.1; weight <= 1; weight += 0.1)
+            {
+                Console.WriteLine("Стоимость {0} кг конфет: {1}", weight, weight * price);
+            }
+        }
+    }
+}
