1362f919b95309da781232de4b100f9d68dfb8e1
masha1321 <mariazajko788@gmail.com>
2024-04-16 13:02:00 +0300

    Task2

---
+++ Topic_1/Topic_1/Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите длину стороны a: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите длину стороны b: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите длину стороны c: ");
+            double c = Convert.ToDouble(Console.ReadLine());
+
+            if (IsRightTriangle(a, b, c))
+                Console.WriteLine("Треугольник является прямоугольным.");
+            else
+                Console.WriteLine("Треугольник не является прямоугольным.");
+        }
+
+        static bool IsRightTriangle(double a, double b, double c)
+        {
+            double[] sides = new double[] { a, b, c };
+            Array.Sort(sides);
+
+            return Math.Pow(sides[0], 2) + Math.Pow(sides[1], 2) == Math.Pow(sides[2], 2);
+        }
+    }
+}
---
+++ Topic_1/Topic_1/Task3/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите номер масти (1-4): ");
+            int m = Convert.ToInt32(Console.ReadLine());
+
+            switch (m)
+            {
+                case 1:
+                    Console.WriteLine("Пики");
+                    break;
+                case 2:
+                    Console.WriteLine("Трефы");
+                    break;
+                case 3:
+                    Console.WriteLine("Бубны");
+                    break;
+                case 4:
+                    Console.WriteLine("Червы");
+                    break;
+                default:
+                    Console.WriteLine("Номер масти должен быть в диапазоне от 1 до 4.");
+                    break;
+            }
+        }
+    }
+}
