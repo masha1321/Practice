d9bfb22a011c153dcd36f531825afe64e4b38c3c
masha1321 <mariazajko788@gmail.com>
2024-04-16 14:07:36 +0300

    Task6

---
+++ Topic_1/Topic_1/Task6/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Task6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double A = 0;
+            double B = 3 * Math.PI;
+            int M = 10;
+            double H = (B - A) / M;
+
+            for (int i = 0; i <= M; i++)
+            {
+                double x = A + H * i;
+                double F = x * Math.Sin(x);
+                Console.WriteLine($"x = {x}, F(x) = {F}");
+            }
+        }
+    }
+}
