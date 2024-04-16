139746e301396806aa40ee942c734c999ad4e827
masha1321 <mariazajko788@gmail.com>
2024-04-16 13:26:50 +0300

    Task4.1

---
+++ Topic_1/Topic_1/Task4.2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Task4._2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int A = 10; 
+            int B = 30; 
+
+            int i = A;
+            do
+            {
+                if (i % 10 % 2 == 0) 
+                {
+                    Console.WriteLine(i);
+                }
+                i++;
+            }
+            while (i <= B);
+        }
+    }
+}
---
+++ Topic_1/Topic_1/Task4/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 1; 
+            int B = 10; 
+
+            while (A <= B)
+            {
+                if (A % 2 == 0) 
+                {
+                    Console.WriteLine(A);
+                }
+                A++;
+            }
+        }
+    }
+}
