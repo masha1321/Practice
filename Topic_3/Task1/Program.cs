0cbde30a8fa17092982479ac8b29ab0a0aeb3077
masha1321 <mariazajko788@gmail.com>
2024-04-18 08:50:19 +0300

    Task1

---
+++ Topic_3/Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Task1
+{
+    class Program
+    {
+        static void Minmax(ref double X, ref double Y)
+        {
+            if (X > Y)
+            {
+                double temp = X;
+                X = Y;
+                Y = temp;
+            }
+        }
+
+        static void Main()
+        {
+            double A = 10, B = 20, C = 30, D = 40;
+            Minmax(ref A, ref B);
+            Minmax(ref A, ref C);
+            Minmax(ref A, ref D);
+            Minmax(ref B, ref C);
+            Minmax(ref B, ref D);
+            Minmax(ref C, ref D);
+
+            Console.WriteLine("Минимальное число: " + A);
+            Console.WriteLine("Максимальное число: " + D);
+        }
+    }
+}
