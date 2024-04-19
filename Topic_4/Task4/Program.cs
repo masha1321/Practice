29ca8858189dbafd4c84904b7ebf4aab41283528
masha1321 <mariazajko788@gmail.com>
2024-04-19 15:09:03 +0300

    Task4
    После замены типа переменных на double, ввод значения 0 для обеих переменных x и y не вызовет ошибок, и на экран будет выведено 0.0. Это связано с тем, что тип double может представлять дробные числа, включая ноль.

---
+++ Topic_4/Task4/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int x = int.Parse(Console.ReadLine());
+                int y = 1 / x;
+                Console.WriteLine(" y ={ 0}", y);
+
+                Console.WriteLine("блок try выполнилсь успешно");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Ошибка: введено нечисловое значение!");
+            }
+            catch (DivideByZeroException) 
+            {
+                Console.WriteLine("Ошибка: деление на 0!");
+
+            }
+
+            Console.WriteLine("конец программы");
+        }
+    }
+}
