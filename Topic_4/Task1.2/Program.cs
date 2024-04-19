926ef3dc463aad41634dab4431b8038ac34eeda6
masha1321 <mariazajko788@gmail.com>
2024-04-19 13:52:41 +0300

    Task1

---
+++ Topic_4/Task1.2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Task1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 10;
+double y;
+            try
+            {
+                if (Math.Tan(x - 1) == 0)
+                    throw new DivideByZeroException();
+                y = 1 / Math.Pow(Math.Tan(x - 1), 2);
+                Console.WriteLine(y);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Ошибка: деление на ноль");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Ошибка: неверный формат");
+            }
+        }
+    }
+}
---
+++ Topic_4/Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 9;
+            double y;
+            try
+            {
+                if (Math.Sin(x) == 0)
+                    throw new DivideByZeroException();
+                y = 2 * x / Math.Sin(x) - 4 * x;
+                Console.WriteLine(y);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Ошибка: деление на ноль");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Ошибка: неверный формат");
+            }
+        }
+    }
+}
