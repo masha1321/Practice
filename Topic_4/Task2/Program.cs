dd49b58f9b56c523787e1b23661652a6342d3b60
masha1321 <mariazajko788@gmail.com>
2024-04-19 14:12:48 +0300

    Task2

--- Topic_4/Task2/Program.cs
+++ Topic_4/Task2/Program.cs
@@ -1,10 +1,51 @@
-﻿namespace Task2
+﻿using System;
+namespace Task2
 {
-    internal class Program
+    class Program
     {
-        static void Main(string[] args)
+        static double CalculateFunction(double x)
         {
-            Console.WriteLine("Hello, World!");
+            if (x <= -4)
+            {
+               
+                if (Math.Abs(2 * x + 1) < double.Epsilon)
+                {
+                    throw new DivideByZeroException("Division by zero");
+                }
+                return x / (2 * x + 1);
+            }
+            else if (x > -1)
+            {
+                return x + 3;
+            }
+            else
+            {
+                return x; 
+            }
+        }
+
+        static void Main()
+        {
+            try
+            {
+                Console.Write("Введите значение x: ");
+                double x = double.Parse(Console.ReadLine());
+
+                double result = CalculateFunction(x);
+                Console.WriteLine($"Значение функции f({x}) = {result}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Ошибка формата ввода. Введите вещественное число.");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Ошибка: деление на ноль.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Произошла ошибка: {ex.Message}");
+            }
         }
     }
 }
