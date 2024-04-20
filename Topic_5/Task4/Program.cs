36f374cf18bb95bfb05270eededa95dc06a47719
masha1321 <mariazajko788@gmail.com>
2024-04-20 08:34:00 +0300

    Task4

--- Topic_5/Task4/Program.cs
+++ Topic_5/Task4/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[,] array = new int[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+
+            int k = 1; 
+            int s = 2; 
+
+          
+            int rowSum = 0;
+            for (int i = 0; i < array.GetLength(1); i++)
+            {
+                rowSum += array[k, i];
+            }
+
+          
+            int columnSum = 0;
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                columnSum += array[i, s];
+            }
+
+            int maxSum = Math.Max(rowSum, columnSum);
+
+            Console.WriteLine("Максимальное из двух чисел: " + maxSum);
         }
     }
 }
