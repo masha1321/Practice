7015711242eb03473e5383ba519ea0e5010d8bce
masha1321 <mariazajko788@gmail.com>
2024-04-20 08:42:46 +0300

    Task5

---
+++ Topic_5/Task5/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            double result = Calculate(n);
+            Console.WriteLine($"Результат: {result}");
+        }
+
+        static double Calculate(int n)
+        {
+            if (n == 0 || n == 1)
+                return 3; 
+            else
+            {
+                double factorial = Factorial(n);
+                return (factorial + 2) / factorial;
+            }
+        }
+
+        static double Factorial(int n)
+        {
+            if (n == 0)
+                return 1;
+            else
+                return n * Factorial(n - 1);
+        }
+    }
+}
