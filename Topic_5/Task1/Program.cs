b69377cfafb123466bdbeb1c6da1ff499ebe14f4
masha1321 <mariazajko788@gmail.com>
2024-04-20 08:18:15 +0300

    Task1

---
+++ Topic_5/Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] array = { 1.2, 3.4, 5.6, 7.8, 9.0 };
+            int index = Array.IndexOf(array, array.Max());
+
+            Console.WriteLine("Порядковый номер максимального элемента: " + (index + 1));
+        }
+    }
+}
