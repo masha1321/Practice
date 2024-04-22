db29b0d76066bc93b81bd8bbc69021cdd6e9866c
masha1321 <mariazajko788@gmail.com>
2024-04-22 11:35:11 +0300

    Task2

---
+++ Topic_6/Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Маша ела кашку";
+            string[] words = text.Split(' ');
+            string longestWord = words.OrderByDescending(s => s.Length).First();
+            Console.WriteLine("Слово максимальной длины: " + longestWord);
+            Console.WriteLine("Количество символов в слове: " + longestWord.Length);
+        }
+    }
+}
