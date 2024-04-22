08db726ecd426989788d37d0ed88366045e36d79
masha1321 <mariazajko788@gmail.com>
2024-04-22 11:29:06 +0300

    Task1

---
+++ Topic_6/Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "Введите ваше сообщение здесь";
+            int n = 5; 
+
+            var words = message.Split(' ')
+                               .Where(word => word.Length <= n);
+
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
