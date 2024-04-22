e70bac8dafc55b634a91929fc5b4e9ed555e7d04
masha1321 <mariazajko788@gmail.com>
2024-04-22 11:43:32 +0300

    Task3

---
+++ Topic_6/Task3/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение:");
+            string sentence = Console.ReadLine();
+            string[] words = sentence.Split(' ');
+
+            string temp = words[0];
+            words[0] = words[words.Length - 1];
+            words[words.Length - 1] = temp;
+
+          
+            if (words.Length > 2)
+            {
+                words[1] = words[1] + words[2];
+                words = words.Where((source, index) => index != 2).ToArray();
+            }
+
+            if (words.Length > 2)
+            {
+                char[] arr = words[2].ToCharArray();
+                Array.Reverse(arr);
+                words[2] = new string(arr);
+            }
+
+            if (words[0].Length > 2)
+            {
+                words[0] = words[0].Substring(2);
+            }
+
+            Console.WriteLine("Результат: " + string.Join(" ", words));
+        }
+    }
+}
