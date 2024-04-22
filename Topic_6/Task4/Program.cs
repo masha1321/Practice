a3a427eaa3c738b920fd81b52626429ab119968e
masha1321 <mariazajko788@gmail.com>
2024-04-22 11:48:37 +0300

    Task4

---
+++ Topic_6/Task4/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите ФИО: ");
+            string fullName = Console.ReadLine();
+            Console.WriteLine("Код личности: " + GetPersonalityCode(fullName));
+        }
+
+        public static int GetPersonalityCode(string fullName)
+        {
+            int sum = fullName.ToUpper()
+                              .Where(char.IsLetter)
+                              .Select(c => c - 'А' + 1)
+                              .Sum();
+
+            while (sum > 9)
+            {
+                sum = sum.ToString().Select(c => c - '0').Sum();
+            }
+
+            return sum;
+        }
+    }
+}
