652eda42dd9b91e1cc439a78f2096c14c9c5b974
masha1321 <mariazajko788@gmail.com>
2024-04-23 09:14:29 +0300

    Task4

---
+++ Topic_7/Task4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Ваш США, ООН, НАТО и т.д.";
+            string pattern = @"\b[A-ZА-ЯЁ]{2,}\b";
+
+            MatchCollection matches = Regex.Matches(text, pattern);
+            foreach (Match match in matches)
+            {
+                Console.WriteLine(match.Value);
+            }
+        }
+    }
+}
