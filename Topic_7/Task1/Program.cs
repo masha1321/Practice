5b85da76278e9205a474be388576f77f288eaf3c
masha1321 <mariazajko788@gmail.com>
2024-04-23 08:45:04 +0300

    Task1

---
+++ Topic_7/Task1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Очень красивые поля и виды, горы и массивы 12 34 217";
+
+            var words = Regex.Matches(text, @"\b\w+\b");
+
+            Console.WriteLine("Слова с четным количеством букв:");
+            foreach (Match word in words)
+            {
+                if (word.Length % 2 == 0)
+                {
+                    Console.WriteLine(word.Value);
+                }
+            }
+
+            Console.WriteLine("\nСлова с нечетным количеством цифр:");
+            foreach (Match word in words)
+            {
+                var digitCount = Regex.Matches(word.Value, @"\d").Count;
+                if (digitCount % 2 != 0)
+                {
+                    Console.WriteLine(word.Value);
+                }
+            }
+        }
+    }
+}
