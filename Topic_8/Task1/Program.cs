15e7a1706a84921e35a8749a80ad5785074370ee
masha1321 <mariazajko788@gmail.com>
2024-04-26 12:54:42 +0300

    Task1

---
+++ Topic_8/Task1/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Task1
+{
+    public enum Post
+    {
+        Worker,
+        Manager,
+       
+    }
+
+    public class Accauntant
+    {
+        private const int StandardHours = 160; 
+
+        public bool AskForBonus(Post worker, int hours)
+        {
+            return hours > StandardHours;
+        }
+
+        static void Main(string[] args)
+        {
+            Accauntant accauntant = new Accauntant();
+            Post worker = Post.Worker;
+            int hours = 170; 
+
+            bool isBonusGranted = accauntant.AskForBonus(worker, hours);
+
+            Console.WriteLine($"Сотрудник: {worker}, Отработанные часы: {hours}");
+            Console.WriteLine($"Премия положена: {isBonusGranted}");
+        }
+    }
+}
