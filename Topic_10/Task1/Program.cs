33935daf66f90648fa04bb8b09d7a38d6b7bd636
masha1321 <mariazajko788@gmail.com>
2024-04-29 15:15:43 +0300

    Task1

---
+++ Topic_10/Task1/Program.cs
@@ -0,0 +1,65 @@
+﻿
+namespace Task1
+{
+        public class Телефон
+        {
+            private int k; // количество функций
+
+            public Телефон(int k)
+            {
+                this.k = k;
+            }
+
+            public int GetK()
+            {
+                return this.k;
+            }
+
+            public void SetK(int k)
+            {
+                this.k = k;
+            }
+
+            public virtual double Стоимость()
+            {
+                return 40 * Math.Log(k);
+            }
+        }
+
+        public class Сотовый : Телефон
+        {
+            private string модель; // модель
+
+            public Сотовый(int k, string модель) : base(k)
+            {
+                this.модель = модель;
+            }
+
+            public string GetМодель()
+            {
+                return this.модель;
+            }
+
+            public void SetМодель(string модель)
+            {
+                this.модель = модель;
+            }
+
+            public override double Стоимость()
+            {
+                return base.Стоимость() * 3;
+            }
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Телефон телефон = new Телефон(5);
+                Console.WriteLine("Телефон стоимость: " + телефон.Стоимость());
+
+                Сотовый сотовый = new Сотовый(5, "iPhone");
+                Console.WriteLine("Сотовый стоимость: " + сотовый.Стоимость());
+            }
+        }
+    }
