0871d482a4de8f9aeb2d0d6cb782934b47b758bf
masha1321 <mariazajko788@gmail.com>
2024-05-03 12:30:33 +0300

    Task3

---
+++ Topic_13/Task3/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Task3
+{
+    public class MyInfo
+    {
+        // Событие, которое вызывается при изменении имени
+        public event Action<string> NameChanged;
+
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    // Вызов события при изменении имени
+                    NameChanged?.Invoke(_name);
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            MyInfo myInfo = new MyInfo();
+
+            // Подписка на событие
+            myInfo.NameChanged += name => Console.WriteLine($"Имя было изменено на: {name}");
+
+            // Изменение имени, что приведет к вызову события
+            myInfo.Name = "Мария";
+        }
+    }
+}
