b986e83744cc52702945f44fc388dd9520150e09
masha1321 <mariazajko788@gmail.com>
2024-05-03 12:26:25 +0300

    Task1

---
+++ Topic_13/Task1/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Task1
+{
+    class Program
+    {
+        
+        delegate double TriangleDelegate(double side);
+
+        static void Main()
+        {
+            try
+            {
+                double side = 5.0;
+
+                TriangleDelegate triangleDelegate = Perimeter;
+                Console.WriteLine($"Периметр равностороннего треугольника со стороной {side} равен {triangleDelegate(side)}");
+
+                triangleDelegate = Area;
+                Console.WriteLine($"Площадь равностороннего треугольника со стороной {side} равна {triangleDelegate(side)}");
+
+                triangleDelegate = Side;
+                Console.WriteLine($"Сторона равностороннего треугольника равна {triangleDelegate(side)}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Произошла ошибка: {ex.Message}");
+            }
+        }
+
+        
+        static double Perimeter(double side)
+        {
+            return 3 * side;
+        }
+
+        
+        static double Area(double side)
+        {
+            return Math.Sqrt(3) / 4 * Math.Pow(side, 2);
+        }
+
+        static double Side(double side)
+        {
+            return side;
+        }
+    }
+}
---
+++ Topic_13/Task2/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Task2
+{
+    class Program
+    {
+        delegate double TriangleDelegate(double side);
+
+        static void Main()
+        {
+            try
+            {
+                double side = 5.0;
+
+                CallDelegate(Perimeter, side);
+                CallDelegate(Area, side);
+                CallDelegate(Side, side);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Произошла ошибка: {ex.Message}");
+            }
+        }
+
+        static double Perimeter(double side)
+        {
+            return 3 * side;
+        }
+
+        static double Area(double side)
+        {
+            return Math.Sqrt(3) / 4 * Math.Pow(side, 2);
+        }
+
+        static double Side(double side)
+        {
+            return side;
+        }
+
+        static void CallDelegate(TriangleDelegate triangleDelegate, double side)
+        {
+            double result = triangleDelegate(side);
+            Console.WriteLine($"Результат: {result}");
+        }
+    }
+}
