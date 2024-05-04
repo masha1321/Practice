c33e42dc5d33762a0b0627dc56159b9df943a107
masha1321 <mariazajko788@gmail.com>
2024-05-04 09:19:31 +0300

    Task1

---
+++ Topic_15/Task1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task1
+{
+    public class MyList<T>
+    {
+        private List<T> _list;
+
+        public MyList()
+        {
+            _list = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            _list.Add(item);
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                try
+                {
+                    return _list[index];
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    Console.WriteLine("Ошибка: Индекс вне диапазона!");
+                    return default(T);
+                }
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return _list.Count;
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> myList = new MyList<int>();
+            myList.Add(1);
+            myList.Add(2);
+            myList.Add(3);
+            Console.WriteLine(myList.Count); 
+        }
+    }
+}
