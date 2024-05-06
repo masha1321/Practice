f7c4c2cd60a44a5d73ecfa4ca633534cc52647e8
masha1321 <mariazajko788@gmail.com>
2024-05-06 14:56:13 +0300

    Task6

---
+++ Topic_16/Task6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите предложение:");
+        string sentence = Console.ReadLine();
+           
+        string result = new string(sentence.Where(c => !Char.IsDigit(c)).ToArray());
+            
+        File.WriteAllText("output.txt", result);
+
+        string fileContent = File.ReadAllText("output.txt");
+        Console.WriteLine("Содержимое файла:");
+        Console.WriteLine(fileContent);
+    }
+}
