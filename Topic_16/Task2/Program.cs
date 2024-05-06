abcdee6758ff903d51198f204d7e9d405c2fa038
masha1321 <mariazajko788@gmail.com>
2024-05-06 14:32:37 +0300

    Task3

---
+++ Topic_16/Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+      
+        string path = @"D:\с рабочего стола\Практика C#\Practice\Topic_16\Task2\New_folder";
+
+        if (!Directory.Exists(path))
+        {
+            Directory.CreateDirectory(path);
+        }
+    }
+}
+
---
+++ Topic_16/Task3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+       
+        string[] lines = { "Первая строка", "Вторая строка", "Третья строка", "Четвертая строка", "Пятая строка" };
+        File.WriteAllLines("input.txt", lines);
+
+        Console.WriteLine(File.ReadAllText("input.txt"));
+
+        Console.WriteLine($"Количество строк: {lines.Length}");
+
+        foreach (var line in lines)
+        {
+            Console.WriteLine($"Количество символов в строке '{line}': {line.Length}");
+        }
+
+        File.WriteAllLines("output.txt", lines.Take(lines.Length - 1));
+
+        int s1 = 1, s2 = 3;
+        Console.WriteLine(string.Join("\n", lines.Skip(s1 - 1).Take(s2 - s1 + 1)));
+
+        var longestLine = lines.OrderByDescending(line => line.Length).First();
+        Console.WriteLine($"Самая длинная строка: '{longestLine}', длина: {longestLine.Length}");
+
+        char startChar = 'В';
+        foreach (var line in lines.Where(line => line.StartsWith(startChar)))
+        {
+            Console.WriteLine(line);
+        }
+
+        File.WriteAllLines("reversed.txt", lines.Reverse());
+    }
+}
