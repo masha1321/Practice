c5cdd376f9b4bb291c5e3fcdc0cbab5da90e5819
masha1321 <mariazajko788@gmail.com>
2024-05-06 14:09:32 +0300

    Task1

---
+++ Topic_16/Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        string filePath = "D:\\с рабочего стола\\Практика C#\\Practice\\Topic_16\\Task1\\files\\f.txt"; 
+        var numbers = File.ReadAllLines(filePath).Select(double.Parse).ToList();
+
+        double sum = numbers.Sum();
+        double difference = numbers.First() - numbers.Last();
+
+        Console.WriteLine("Сумма чисел: " + sum);
+        Console.WriteLine("Разность первого и последнего числа: " + difference);
+    }
+}
