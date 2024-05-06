3f4061e9dd6046eac71ca6f441db59746d8a1081
masha1321 <mariazajko788@gmail.com>
2024-05-06 14:46:25 +0300

    Task4

---
+++ Topic_16/Task4/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        var lines = File.ReadAllLines("D:\\с рабочего стола\\Практика C#\\Practice\\Topic_16\\Task4\\files\\1.txt");
+        File.WriteAllLines("D:\\с рабочего стола\\Практика C#\\Practice\\Topic_16\\Task4\\files\\2.txt", lines.Where((line, index) => index % 2 != 0));
+        File.WriteAllLines("D:\\с рабочего стола\\Практика C#\\Practice\\Topic_16\\Task4\\files\\3.txt", lines.Where((line, index) => index % 2 == 0));
+    }
+}
