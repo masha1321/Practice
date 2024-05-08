51a4a5829d22a2ac0c553b1cfd887ff886c74e93
masha1321 <mariazajko788@gmail.com>
2024-05-08 09:33:54 +0300

    Task2

---
+++ Topic_18/Task2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        double a = 10; 
+        double b = 20; 
+
+        string path = "D:\\с рабочего стола\\Практика C#\\Practice\\Topic_18\\Task2\\numbers.txt"; 
+
+        var numbers = File.ReadAllLines(path)
+                          .SelectMany(line => line.Split(' '))
+                          .Select(double.Parse)
+                          .ToList();
+
+        var inRange = new List<double>();
+        var lessThanA = new List<double>();
+        var greaterThanB = new List<double>();
+
+        foreach (var number in numbers)
+        {
+            if (number >= a && number <= b)
+            {
+                inRange.Add(number);
+            }
+            else if (number < a)
+            {
+                lessThanA.Add(number);
+            }
+            else
+            {
+                greaterThanB.Add(number);
+            }
+        }
+
+        Console.WriteLine("Числа из интервала [a, b]: " + string.Join(", ", inRange));
+        Console.WriteLine("Числа меньше a: " + string.Join(", ", lessThanA));
+        Console.WriteLine("Числа больше b: " + string.Join(", ", greaterThanB));
+    }
+}
