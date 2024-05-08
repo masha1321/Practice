a08c7d557c8d45a803865ea25da9842066e07335
masha1321 <mariazajko788@gmail.com>
2024-05-08 09:08:41 +0300

    Task1

---
+++ Topic_18/Task1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        string input = "abc#d##c";
+        Console.WriteLine("Исходная строка: " + input);
+        Console.WriteLine("Преобразованная строка: " + TransformString(input));
+    }
+
+    static string TransformString(string input)
+    {
+        Stack<char> stack = new Stack<char>();
+
+        foreach (char c in input)
+        {
+            if (c != '#')
+            {
+                stack.Push(c);
+            }
+            else if (stack.Count > 0)
+            {
+                stack.Pop();
+            }
+        }
+
+        char[] result = stack.ToArray();
+        Array.Reverse(result);
+        return new string(result);
+    }
+}
