4667d47178661c40a2d3e81202c85bdd2243c8ff
masha1321 <mariazajko788@gmail.com>
2024-05-08 11:34:55 +0300

    Task10

---
+++ Topic19/Task10/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    /// <summary>
+    /// Главный метод программы.
+    /// </summary>
+    static void Main()
+    {
+        // Путь к файлу
+        string filePath = "D:\\с рабочего стола\\Практика C#\\Practice\\Topic_16\\Task1\\files\\f.txt";
+
+        // Чтение чисел из файла и преобразование их в список
+        var numbers = File.ReadAllLines(filePath).Select(double.Parse).ToList();
+
+        // Вычисление суммы чисел
+        double sum = numbers.Sum();
+
+        // Вычисление разности первого и последнего числа
+        double difference = numbers.First() - numbers.Last();
+
+        // Вывод результатов
+        Console.WriteLine("Сумма чисел: " + sum);
+        Console.WriteLine("Разность первого и последнего числа: " + difference);
+    }
+}
