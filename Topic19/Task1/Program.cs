8b2a1b929cd3917f1a0b66dff3b0a50cdf0b40e8
masha1321 <mariazajko788@gmail.com>
2024-05-08 10:54:48 +0300

    Task1

---
+++ Topic19/Task1/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Task1
+{
+    /// <summary>
+    /// Главный класс программы.
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Метод для обмена значений двух переменных, если первая больше второй.
+        /// </summary>
+        /// <param name="X">Первое число.</param>
+        /// <param name="Y">Второе число.</param>
+        static void Minmax(ref double X, ref double Y)
+        {
+            if (X > Y)
+            {
+                double temp = X;
+                X = Y;
+                Y = temp;
+            }
+        }
+        /// <summary>
+        /// Главный метод программы.
+        /// </summary>
+        static void Main()
+        {
+            double A = 10, B = 20, C = 30, D = 40;
+            Minmax(ref A, ref B);
+            Minmax(ref A, ref C);
+            Minmax(ref A, ref D);
+            Minmax(ref B, ref C);
+            Minmax(ref B, ref D);
+            Minmax(ref C, ref D);
+
+            Console.WriteLine("Минимальное число: " + A);
+            Console.WriteLine("Максимальное число: " + D);
+        }
+    }
+}
