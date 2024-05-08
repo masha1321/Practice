9988b733d7c2f01e0c142930b296b34012fc366d
masha1321 <mariazajko788@gmail.com>
2024-05-08 11:36:57 +0300

    Task11

---
+++ Topic19/Task11/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+
+/// <summary>
+/// Главный класс программы.
+/// </summary>
+class Program
+{
+    /// <summary>
+    /// Главный метод программы.
+    /// </summary>
+    static void Main()
+    {
+        // Путь к папке
+        string path = @"D:\с рабочего стола\Практика C#\Practice\Topic_16\Task2\New_folder";
+
+        // Проверка на существование папки и создание, если она отсутствует
+        if (!Directory.Exists(path))
+        {
+            Directory.CreateDirectory(path);
+        }
+    }
+}
