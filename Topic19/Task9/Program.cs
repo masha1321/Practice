ba5eeb0f151d387bd9da66f6f2f40c3fb613a66d
masha1321 <mariazajko788@gmail.com>
2024-05-08 11:31:26 +0300

    Task9

---
+++ Topic19/Task9/Program.cs
@@ -0,0 +1,37 @@
+﻿class Program
+{
+    /// <summary>
+    /// Делегат для генерации случайного числа.
+    /// </summary>
+    delegate int RandomNumberDelegate();
+
+    /// <summary>
+    /// Главный метод программы.
+    /// </summary>
+    static void Main()
+    {
+        // Создание массива делегатов
+        RandomNumberDelegate[] delegates = new RandomNumberDelegate[5];
+        Random random = new Random();
+
+        // Инициализация делегатов анонимной функцией, генерирующей случайное число
+        for (int i = 0; i < delegates.Length; i++)
+        {
+            delegates[i] = delegate () { return random.Next(1, 100); };
+        }
+
+        // Создание делегата для вычисления среднего значения
+        Func<RandomNumberDelegate[], double> average = delegate (RandomNumberDelegate[] dels)
+        {
+            double sum = 0;
+            foreach (var del in dels)
+            {
+                sum += del();
+            }
+            return sum / dels.Length;
+        };
+
+        // Вывод среднего значения
+        Console.WriteLine("Среднее значение: " + average(delegates));
+    }
+}
