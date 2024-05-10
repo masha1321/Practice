29ae543937b6ae79bce00ffd6345a76596ddf93f
masha1321 <mariazajko788@gmail.com>
2024-05-10 10:16:13 +0300

    Task5

---
+++ Topic_20/Task5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = { 65, 2, 6, 87 }; 
+        int sumLimit = 100; 
+        int productLimit = 1000; 
+
+        Parallel.ForEach(array, (n, state) =>
+        {
+            int sum = 0;
+            int product = 1;
+            for (int i = 0; i <= n; i++)
+            {
+                sum += i;
+                product *= i > 0 ? i : 1;
+
+                
+                if (sum > sumLimit || product > productLimit)
+                {
+                    Console.WriteLine($"Прерывание выполнения для N = {n}. Сумма = {sum}, Произведение = {product}");
+                    state.Break();
+                    return;
+                }
+            }
+
+            Console.WriteLine($"N = {n}. Сумма = {sum}, Произведение = {product}");
+        });
+    }
+}
