cce1cb493809cb4029970c57e7a6202de73a469c
masha1321 <mariazajko788@gmail.com>
2024-05-11 21:57:34 +0300

    Task1

---
+++ Topic_21/Task1/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Task1
+{
+    public interface IDogBehavior
+    {
+        void Act();
+    }
+
+    public class BarkBehavior : IDogBehavior
+    {
+        public void Act()
+        {
+            Console.WriteLine("Гав-гав");
+        }
+    }
+
+    public class HowlBehavior : IDogBehavior
+    {
+        public void Act()
+        {
+            Console.WriteLine("Аф-Аф");
+        }
+    }
+
+    public class Dog
+    {
+        private IDogBehavior _behavior;
+
+        public Dog(IDogBehavior initialBehavior)
+        {
+            _behavior = initialBehavior;
+        }
+
+        public void SetBehavior(IDogBehavior newBehavior)
+        {
+            _behavior = newBehavior;
+        }
+
+        public void PerformAct()
+        {
+            _behavior.Act();
+        }
+    }
+
+        class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog myDog = new Dog(new BarkBehavior());
+            myDog.PerformAct(); 
+
+            myDog.SetBehavior(new HowlBehavior());
+            myDog.PerformAct(); 
+        }
+    }
+}
